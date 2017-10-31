d3d90efbb39efb4fba49aed94cab74298ea783e9
Reid Williams <rwilliams@ideo.com>
2017-10-31 14:45:17 -0700

    still working

--- model.py
+++ model.py
@@ -53,10 +53,6 @@ class DCGAN(object):
         self.sample_size = sample_size
         self.image_shape = [image_size, image_size, c_dim]
 
-        self.lowres = lowres
-        self.lowres_size = image_size // lowres
-        self.lowres_shape = [self.lowres_size, self.lowres_size, c_dim]
-
         self.z_dim = z_dim
 
         self.gf_dim = gf_dim
@@ -86,23 +82,12 @@ class DCGAN(object):
         self.is_training = tf.placeholder(tf.bool, name='is_training')
         self.images = tf.placeholder(
             tf.float32, [None] + self.image_shape, name='real_images')
-        self.lowres_images = tf.reduce_mean(tf.reshape(self.images,
-            [self.batch_size, self.lowres_size, self.lowres,
-             self.lowres_size, self.lowres, self.c_dim]), [2, 4])
+      
         self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
-        self.z_sum = tf.summary.histogram("z", self.z)
 
         self.G = self.generator(self.z)
-        self.lowres_G = tf.reduce_mean(tf.reshape(self.G,
-            [self.batch_size, self.lowres_size, self.lowres,
-             self.lowres_size, self.lowres, self.c_dim]), [2, 4])
         self.D, self.D_logits = self.discriminator(self.images)
-
         self.D_, self.D_logits_ = self.discriminator(self.G, reuse=True)
-
-        self.d_sum = tf.summary.histogram("d", self.D)
-        self.d__sum = tf.summary.histogram("d_", self.D_)
-        self.G_sum = tf.summary.image("G", self.G)
 
         self.d_out_real_sum = tf.summary.scalar("d_out_real", tf.reduce_mean(self.D))
         self.d_out_fake_sum = tf.summary.scalar("d_out_fake", tf.reduce_mean(self.D_))
@@ -117,13 +102,13 @@ class DCGAN(object):
             tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_logits_,
                                                     labels=tf.ones_like(self.D_)))
 
-        self.d_loss_real_sum = tf.summary.scalar("d_loss_real", self.d_loss_real)
-        self.d_loss_fake_sum = tf.summary.scalar("d_loss_fake", self.d_loss_fake)
-
         self.d_loss = self.d_loss_real + self.d_loss_fake
 
-        self.g_loss_sum = tf.summary.scalar("g_loss", self.g_loss)
-        self.d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
+        # self.d_loss_real_sum = tf.summary.scalar("d_loss_real", self.d_loss_real)
+        # self.d_loss_fake_sum = tf.summary.scalar("d_loss_fake", self.d_loss_fake)
+
+        # self.g_loss_sum = tf.summary.scalar("g_loss", self.g_loss)
+        # self.d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
 
         t_vars = tf.trainable_variables()
 
@@ -131,19 +116,6 @@ class DCGAN(object):
         self.g_vars = [var for var in t_vars if 'g_' in var.name]
 
         self.saver = tf.train.Saver(max_to_keep=1)
-
-        # Completion.
-        self.mask = tf.placeholder(tf.float32, self.image_shape, name='mask')
-        self.lowres_mask = tf.placeholder(tf.float32, self.lowres_shape, name='lowres_mask')
-        self.contextual_loss = tf.reduce_sum(
-            tf.contrib.layers.flatten(
-                tf.abs(tf.multiply(self.mask, self.G) - tf.multiply(self.mask, self.images))), 1)
-        self.contextual_loss += tf.reduce_sum(
-            tf.contrib.layers.flatten(
-                tf.abs(tf.multiply(self.lowres_mask, self.lowres_G) - tf.multiply(self.lowres_mask, self.lowres_images))), 1)
-        self.perceptual_loss = self.g_loss
-        self.complete_loss = self.contextual_loss + self.lam*self.perceptual_loss
-        self.grad_complete_loss = tf.gradients(self.complete_loss, self.z)
 
     def train(self, config):
         data = dataset_files(config.dataset)
@@ -154,15 +126,13 @@ class DCGAN(object):
                           .minimize(self.d_loss, var_list=self.d_vars)
         g_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
                           .minimize(self.g_loss, var_list=self.g_vars)                
+        
         try:
             tf.global_variables_initializer().run()
         except:
             tf.initialize_all_variables().run()
 
-        self.g_sum = tf.summary.merge(
-            [self.z_sum, self.d__sum, self.G_sum, self.d_loss_fake_sum, self.g_loss_sum])
-        self.d_sum = tf.summary.merge(
-            [self.z_sum, self.d_sum, self.d_loss_real_sum, self.d_loss_sum])
+        self.d_sum = tf.summary.merge([self.d_out_real_sum, self.d_out_fake_sum])
         self.writer = tf.summary.FileWriter("./logs", self.sess.graph)
 
         sample_z = np.random.uniform(-1, 1, size=(self.sample_size , self.z_dim))
@@ -173,29 +143,6 @@ class DCGAN(object):
 
         counter = 1
         start_time = time.time()
-
-        if self.load(self.checkpoint_dir):
-            print("""
-
-======
-An existing model was found in the checkpoint directory.
-If you just cloned this repository, it's a model for faces
-trained on the CelebA dataset for 20 epochs.
-If you want to train a new model from scratch,
-delete the checkpoint directory or specify a different
---checkpoint_dir argument.
-======
-
-""")
-        else:
-            print("""
-
-======
-An existing model was not found in the checkpoint directory.
-Initializing a new one.
-======
-
-""")
 
         for epoch in xrange(config.epoch):
             data = dataset_files(config.dataset)
@@ -216,14 +163,14 @@ Initializing a new one.
                 self.writer.add_summary(summary_str, counter)
 
                 # Update G network
-                _, summary_str = self.sess.run([g_optim, self.g_sum],
+                self.sess.run(g_optim,
                     feed_dict={ self.z: batch_z, self.is_training: True })
-                self.writer.add_summary(summary_str, counter)
+                # self.writer.add_summary(summary_str, counter)
 
                 # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-                _, summary_str = self.sess.run([g_optim, self.g_sum],
+                self.sess.run(g_optim,
                     feed_dict={ self.z: batch_z, self.is_training: True })
-                self.writer.add_summary(summary_str, counter)
+                # self.writer.add_summary(summary_str, counter)
 
                 errD_fake = self.d_loss_fake.eval({self.z: batch_z, self.is_training: False})
                 errD_real = self.d_loss_real.eval({self.images: batch_images, self.is_training: False})
@@ -233,7 +180,7 @@ Initializing a new one.
                 print("Epoch: [{:2d}] [{:4d}/{:4d}] time: {:4.4f}, d_loss: {:.8f}, g_loss: {:.8f}".format(
                     epoch, idx, batch_idxs, time.time() - start_time, errD_fake+errD_real, errG))
 
-                if np.mod(counter, 100) == 1:
+                if np.mod(counter, 10) == 1:
                     samples, d_loss, g_loss = self.sess.run(
                         [self.G, self.d_loss, self.g_loss],
                         feed_dict={self.z: sample_z, self.images: sample_images, self.is_training: False}
@@ -244,165 +191,6 @@ Initializing a new one.
 
                 if np.mod(counter, 500) == 2:
                     self.save(config.checkpoint_dir, counter)
-
-
-    def complete(self, config):
-        def make_dir(name):
-            # Works on python 2.7, where exist_ok arg to makedirs isn't available.
-            p = os.path.join(config.outDir, name)
-            if not os.path.exists(p):
-                os.makedirs(p)
-        make_dir('hats_imgs')
-        make_dir('completed')
-        make_dir('logs')
-
-        try:
-            tf.global_variables_initializer().run()
-        except:
-            tf.initialize_all_variables().run()
-
-        isLoaded = self.load(self.checkpoint_dir)
-        assert(isLoaded)
-
-        nImgs = len(config.imgs)
-
-        batch_idxs = int(np.ceil(nImgs/self.batch_size))
-        lowres_mask = np.zeros(self.lowres_shape)
-        if config.maskType == 'random':
-            fraction_masked = 0.2
-            mask = np.ones(self.image_shape)
-            mask[np.random.random(self.image_shape[:2]) < fraction_masked] = 0.0
-        elif config.maskType == 'center':
-            assert(config.centerScale <= 0.5)
-            mask = np.ones(self.image_shape)
-            sz = self.image_size
-            l = int(self.image_size*config.centerScale)
-            u = int(self.image_size*(1.0-config.centerScale))
-            mask[l:u, l:u, :] = 0.0
-        elif config.maskType == 'left':
-            mask = np.ones(self.image_shape)
-            c = self.image_size // 2
-            mask[:,:c,:] = 0.0
-        elif config.maskType == 'full':
-            mask = np.ones(self.image_shape)
-        elif config.maskType == 'grid':
-            mask = np.zeros(self.image_shape)
-            mask[::4,::4,:] = 1.0
-        elif config.maskType == 'lowres':
-            lowres_mask = np.ones(self.lowres_shape)
-            mask = np.zeros(self.image_shape)
-        else:
-            assert(False)
-
-        for idx in xrange(0, batch_idxs):
-            l = idx*self.batch_size
-            u = min((idx+1)*self.batch_size, nImgs)
-            batchSz = u-l
-            batch_files = config.imgs[l:u]
-            batch = [get_image(batch_file, self.image_size, is_crop=self.is_crop)
-                     for batch_file in batch_files]
-            batch_images = np.array(batch).astype(np.float32)
-            if batchSz < self.batch_size:
-                print(batchSz)
-                padSz = ((0, int(self.batch_size-batchSz)), (0,0), (0,0), (0,0))
-                batch_images = np.pad(batch_images, padSz, 'constant')
-                batch_images = batch_images.astype(np.float32)
-
-            zhats = np.random.uniform(-1, 1, size=(self.batch_size, self.z_dim))
-            m = 0
-            v = 0
-
-            nRows = np.ceil(batchSz/8)
-            nCols = min(8, batchSz)
-            save_images(batch_images[:batchSz,:,:,:], [nRows,nCols],
-                        os.path.join(config.outDir, 'before.png'))
-            masked_images = np.multiply(batch_images, mask)
-            save_images(masked_images[:batchSz,:,:,:], [nRows,nCols],
-                        os.path.join(config.outDir, 'masked.png'))
-            if lowres_mask.any():
-                lowres_images = np.reshape(batch_images, [self.batch_size, self.lowres_size, self.lowres,
-                    self.lowres_size, self.lowres, self.c_dim]).mean(4).mean(2)
-                lowres_images = np.multiply(lowres_images, lowres_mask)
-                lowres_images = np.repeat(np.repeat(lowres_images, self.lowres, 1), self.lowres, 2)
-                save_images(lowres_images[:batchSz,:,:,:], [nRows,nCols],
-                            os.path.join(config.outDir, 'lowres.png'))
-            for img in range(batchSz):
-                with open(os.path.join(config.outDir, 'logs/hats_{:02d}.log'.format(img)), 'a') as f:
-                    f.write('iter loss ' +
-                            ' '.join(['z{}'.format(zi) for zi in range(self.z_dim)]) +
-                            '\n')
-
-            for i in xrange(config.nIter):
-                fd = {
-                    self.z: zhats,
-                    self.mask: mask,
-                    self.lowres_mask: lowres_mask,
-                    self.images: batch_images,
-                    self.is_training: False
-                }
-                run = [self.complete_loss, self.grad_complete_loss, self.G, self.lowres_G]
-                loss, g, G_imgs, lowres_G_imgs = self.sess.run(run, feed_dict=fd)
-
-                for img in range(batchSz):
-                    with open(os.path.join(config.outDir, 'logs/hats_{:02d}.log'.format(img)), 'ab') as f:
-                        f.write('{} {} '.format(i, loss[img]).encode())
-                        np.savetxt(f, zhats[img:img+1])
-
-                if i % config.outInterval == 0:
-                    print(i, np.mean(loss[0:batchSz]))
-                    imgName = os.path.join(config.outDir,
-                                           'hats_imgs/{:04d}.png'.format(i))
-                    nRows = np.ceil(batchSz/8)
-                    nCols = min(8, batchSz)
-                    save_images(G_imgs[:batchSz,:,:,:], [nRows,nCols], imgName)
-                    if lowres_mask.any():
-                        imgName = imgName[:-4] + '.lowres.png'
-                        save_images(np.repeat(np.repeat(lowres_G_imgs[:batchSz,:,:,:],
-                                              self.lowres, 1), self.lowres, 2),
-                                    [nRows,nCols], imgName)
-
-                    inv_masked_hat_images = np.multiply(G_imgs, 1.0-mask)
-                    completed = masked_images + inv_masked_hat_images
-                    imgName = os.path.join(config.outDir,
-                                           'completed/{:04d}.png'.format(i))
-                    save_images(completed[:batchSz,:,:,:], [nRows,nCols], imgName)
-
-                if config.approach == 'adam':
-                    # Optimize single completion with Adam
-                    m_prev = np.copy(m)
-                    v_prev = np.copy(v)
-                    m = config.beta1 * m_prev + (1 - config.beta1) * g[0]
-                    v = config.beta2 * v_prev + (1 - config.beta2) * np.multiply(g[0], g[0])
-                    m_hat = m / (1 - config.beta1 ** (i + 1))
-                    v_hat = v / (1 - config.beta2 ** (i + 1))
-                    zhats += - np.true_divide(config.lr * m_hat, (np.sqrt(v_hat) + config.eps))
-                    zhats = np.clip(zhats, -1, 1)
-
-                elif config.approach == 'hmc':
-                    # Sample example completions with HMC (not in paper)
-                    zhats_old = np.copy(zhats)
-                    loss_old = np.copy(loss)
-                    v = np.random.randn(self.batch_size, self.z_dim)
-                    v_old = np.copy(v)
-
-                    for steps in range(config.hmcL):
-                        v -= config.hmcEps/2 * config.hmcBeta * g[0]
-                        zhats += config.hmcEps * v
-                        np.copyto(zhats, np.clip(zhats, -1, 1))
-                        loss, g, _, _ = self.sess.run(run, feed_dict=fd)
-                        v -= config.hmcEps/2 * config.hmcBeta * g[0]
-
-                    for img in range(batchSz):
-                        logprob_old = config.hmcBeta * loss_old[img] + np.sum(v_old[img]**2)/2
-                        logprob = config.hmcBeta * loss[img] + np.sum(v[img]**2)/2
-                        accept = np.exp(logprob_old - logprob)
-                        if accept < 1 and np.random.uniform() > accept:
-                            np.copyto(zhats[img], zhats_old[img])
-
-                    config.hmcBeta *= config.hmcAnneal
-
-                else:
-                    assert(False)
 
     def discriminator(self, image, reuse=False):
         with tf.variable_scope("discriminator") as scope:
